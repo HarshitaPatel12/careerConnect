2abf3a98ab874766aa2d4e3917afa2ec1d0b18d6
Surbhi-Bhayare <byayaresurbhi1998@gmail.com>
2023-12-26 11:53:33 +0530

    component

---
+++ src/Components/SidebarRight/SidebarRight.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function SidebarRight() {
+    return (
+      <div className="SidebarRight">
+         
+      </div>
+    );
+  }
+  
+  export default SidebarRight;
+  
