8f7d7ef0154881fc7d77dc621b26597162252d6b
HarshitaPatel12 <harshitapate919@gmail.com>
2023-12-26 12:40:48 +0530

    login page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Profile from './Components/Profile/Profile';
 import SidebarRight from './Components/SidebarRight/SidebarRight';
+import Login from './Components/login/login';
+
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/profile" element={<Profile />} />
           <Route path="/sidebar" element={<SidebarRight />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
     </div>
